ab9862eed14cdabd7158d7214442c88de94f9b37
Sebastián Ramírez <sramirez10r@gmail.com>
2016-07-25 10:45:20 -0600

    Completing login screen

---
+++ assets/bonsai/bonsai.config.js
@@ -0,0 +1,33 @@
+(function(){
+  'use strict'
+
+  angular
+    .module('bonsai')
+    .config(configBlock);
+
+  function configBlock($mdThemingProvider) {
+    //Generated using http://mcg.mbitson.com/#/
+    $mdThemingProvider.definePalette('bonsaiPrimary', {
+      '50': '#bdcbd6',
+      '100': '#8ea6b9',
+      '200': '#6c8ba3',
+      '300': '#4c667b',
+      '400': '#415768',
+      '500': '#354755',
+      '600': '#293742',
+      '700': '#1d272f',
+      '800': '#12181c',
+      '900': '#06080a',
+      'A100': '#bdcbd6',
+      'A200': '#8ea6b9',
+      'A400': '#415768',
+      'A700': '#1d272f',
+      'contrastDefaultColor': 'light',
+      'contrastDarkColors': '50 100 200 A100 A200'
+    });
+
+    $mdThemingProvider.theme('default')
+    .primaryPalette('bonsaiPrimary')
+    .accentPalette('cyan');
+  }
+})();
