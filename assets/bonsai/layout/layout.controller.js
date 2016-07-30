ca4e49ecb521c5bc6aef05a1026fcb680bf2804e
Sebastián Ramírez <sramirez10r@gmail.com>
2016-07-30 13:13:22 -0600

    Toggling sidebar

--- assets/bonsai/layout/layout.controller.js
+++ assets/bonsai/layout/layout.controller.js
@@ -5,13 +5,17 @@
     .module('bonsai')
     .controller('LayoutCtrl', LayoutCtrl);
 
-    LayoutCtrl.$inject = ['authService'];
+    LayoutCtrl.$inject = ['$mdSidenav','authService'];
 
-    function LayoutCtrl(authService) {
+    function LayoutCtrl($mdSidenav, authService) {
       var vm = this;
 
       function init() {
-        vm.user = authService.getUser();
+        vm.user = JSON.parse(authService.getUser());
+      }
+
+      vm.toggleSidenav = function(navID) {
+        $mdSidenav(navID).toggle();
       }
 
       init();
